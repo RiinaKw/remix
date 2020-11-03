942ab33e1f36ab90eb9442f6f2478fbfd2845344
Riina K <riinak.tv@gmail.com>
2020-11-03 12:24:43 +0900

    Fixed wrong BayTest initialize path

--- demo/tests/BayTest.php
+++ demo/tests/BayTest.php
@@ -13,7 +13,7 @@ class BayTest extends TestCase
     protected function setUp() : void
     {
         require_once(__DIR__ . '/../../vendor/autoload.php');
-        $this->remix = \Remix\App::getInstance()->initialize(__DIR__ . '/../demo');
+        $this->remix = \Remix\App::getInstance()->initialize(__DIR__ . '/..');
     }
 
     public function testInstrument()

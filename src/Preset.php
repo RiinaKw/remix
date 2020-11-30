9d0631378b45467069fc11a9ed1f17084b68184f
Riina K <riinak.tv@gmail.com>
2020-11-30 20:11:57 +0900

    fabpot/php-cs-fixer to friendsofphp/php-cs-fixer

--- src/Preset.php
+++ src/Preset.php
@@ -13,10 +13,11 @@ class Preset extends Component
 
         $remix = App::getInstance();
         $this->dir = $remix->appDir('/Preset');
-        $this->hash = new Utility\Hash;
-    } // function __construct()
+        $this->hash = new Utility\Hash();
+    }
+    // function __construct()
 
-    public function load(string $file, string $key = '') : void
+    public function load(string $file, string $key = ''): void
     {
         $filename = str_replace('.', '/', $file);
         $file = $this->dir . '/' . $filename . '.php';
@@ -31,20 +32,25 @@ class Preset extends Component
         } else {
             $this->hash->set($filename, $preset);
         }
-    } // function load()
+    }
+    // function load()
 
     public function get(string $name = '')
     {
         return $this->hash->get($name);
-    } // function get()
+    }
+    // function get()
 
     public function set(string $name, $value)
     {
         return $this->hash->set($name, $value);
-    } // function get()
+    }
+    // function get()
 
-    public function env() : string
+    public function env(): string
     {
         return $this->hash->get('env.name');
     }
-} // class Preset
+    // function env()
+}
+// class Preset

b978f64031f33261558464a35620edaaef15bb2c
Riina K <riinak.tv@gmail.com>
2021-05-28 23:17:39 +0900

    Refactor the method that sends the status code

--- src/Studio.php
+++ src/Studio.php
@@ -37,6 +37,61 @@ class Studio extends Gear
         ],
     ];
 
+    protected static $status_code = [
+        '200' => 'OK',
+        '201' => 'Created',
+        '202' => 'Accepted',
+        '203' => 'Non-Authoritative Information',
+        '204' => 'No Content',
+        '205' => 'Reset Content',
+        '206' => 'Partial Content',
+
+        '300' => 'Multiple Choice',
+        '301' => 'Moved Permanently',
+        '302' => 'Found',
+        '303' => 'See Other',
+        '304' => 'Not Modified',
+        '307' => 'Temporary Redirect',
+        '308' => 'Permanent Redirect',
+
+        '400' => 'Bad Request',
+        '401' => 'Unauthorized',
+        '402' => 'Payment Required',
+        '403' => 'Forbidden',
+        '404' => 'Not Found',
+        '405' => 'Method Not Allowed',
+        '406' => 'Not Acceptable',
+        '407' => 'Proxy Authentication Required',
+        '408' => 'Request Timeout',
+        '409' => 'Conflict',
+        '410' => 'Gone',
+        '411' => 'Length Required',
+        '412' => 'Precondition Failed',
+        '413' => 'Payload Too Large',
+        '414' => 'URI Too Long',
+        '415' => 'Unsupported Media Type',
+        '416' => 'Range Not Satisfiable',
+        '417' => 'Expectation Failed',
+        '418' => 'I\'m a teapot',
+        '421' => 'Misdirected Request',
+        '425' => 'Too Early',
+        '426' => 'Upgrade Required',
+        '428' => 'Precondition Required',
+        '429' => 'Too Many Requests',
+        '431' => 'Request Header Fields Too Large',
+        '451' => 'Unavailable For Legal Reasons',
+
+        '500' => 'Internal Server Error',
+        '501' => 'Not Implemented',
+        '502' => 'Bad Gateway',
+        '503' => 'Service Unavailable',
+        '504' => 'Gateway Timeout',
+        '505' => 'HTTP Version Not Supported',
+        '506' => 'Variant Also Negotiates',
+        '510' => 'Not Extended',
+        '511' => 'Network Authentication Required',
+    ];
+
     public function __construct(string $type = 'none', $params = [])
     {
         parent::__construct();
@@ -67,6 +122,19 @@ class Studio extends Gear
         Audio::getInstance()->console = $mimetype['console'];
         $this->headers[] = "Content-type: {$mimetype['type']}; charset={$charset}";
     }
+
+    public function status(int $code = 200): self
+    {
+        if (! isset(static::$status_code[$code])) {
+            $code = '500';
+        }
+
+        $message = static::$status_code[$code];
+        $this->headers[] = "HTTP/1.1 {$code} {$message}";
+
+        return $this;
+    }
+    // function status()
 
     protected function sendHeader()
     {
@@ -133,13 +201,6 @@ class Studio extends Gear
         return $output;
     }
     // function recorded()
-
-    public function status(int $code = 200): self
-    {
-        http_response_code($code);
-        return $this;
-    }
-    // function status()
 
     public function redirect(string $name, array $params = [], int $status = 303): self
     {
